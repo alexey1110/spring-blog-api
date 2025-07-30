c294e88b588599b5cc4640262d8b95847397b91c
alexey1110 <osipovalexey123@mail.ru>
2025-07-30 21:53:39 +0400

    add springdoc-openapi for post

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Post;
 import com.example.demo.repository.PostRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "tag_for_posts")
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -17,11 +19,13 @@ public class PostController {
         this.postRepository = postRepository;
     }
 
+    @Tag(name = "get_all_posts")
     @GetMapping
     public List<Post> getPosts(){
         return postRepository.findAll();
     }
 
+    @Tag(name = "create_post")
     @PostMapping
     public Post createPost(@RequestBody Post post){
         return postRepository.save(post);
